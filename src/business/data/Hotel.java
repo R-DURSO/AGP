55d016566debd1bf8d1b41d5d9be49193d616e12
redouanedebart <redouane.debart@gmail.com>
2022-01-14 16:29:19 +0100

    Added the different hotel classes

--- src/business/data/Hotel.java
+++ src/business/data/Hotel.java
@@ -1,5 +1,22 @@
 package business.data;
 
 public abstract class Hotel {
-
+	
+	private int comfortLevel;
+	private int priceLevel;
+	
+	public int getComfortLevel() {
+		return comfortLevel;
+	}
+	public void setComfortLevel(int comfortLevel) {
+		this.comfortLevel = comfortLevel;
+	}
+	public int getPriceLevel() {
+		return priceLevel;
+	}
+	public void setPriceLevel(int priceLevel) {
+		this.priceLevel = priceLevel;
+	}
+	
+	
 }
