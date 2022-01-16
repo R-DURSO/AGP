0f9d4266b14eede98734e921504df4fd4373eb0e
redouanedebart <redouane.debart@gmail.com>
2022-01-16 15:32:28 +0100

    added content two these classes

--- src/business/data/Excursion.java
+++ src/business/data/Excursion.java
@@ -1,5 +1,30 @@
 package business.data;
 
-public class Excursion {
+import java.util.List;
 
+public class Excursion {
+	private List<Site> siteList;
+	private List<Hotel> hotelList;
+	private List<Transport> transportList; //may be use a hashmap to have a specific transport for every site
+	
+	public List<Site> getSiteList() {
+		return siteList;
+	}
+	public void setSiteList(List<Site> siteList) {
+		this.siteList = siteList;
+	}
+	public List<Hotel> getHotelList() {
+		return hotelList;
+	}
+	public void setHotelList(List<Hotel> hotelList) {
+		this.hotelList = hotelList;
+	}
+	public List<Transport> getTransportList() {
+		return transportList;
+	}
+	public void setTransportList(List<Transport> transportList) {
+		this.transportList = transportList;
+	}
+	
+	
 }
--- src/business/data/Site.java
+++ src/business/data/Site.java
@@ -1,5 +1,16 @@
 package business.data;
 
 public class Site {
+	private String description;
+	//ajouter la localisation geo ?
 
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 }
