d11f53ba7607a80156e181e8d902bf22c7d8ed76
R-DURSO <rdurso@outlook.fr>
2022-01-18 11:48:36 +0100

    premier jet méthodes pour la création des excurtion

--- src/core/Sejour.java
+++ src/core/Sejour.java
@@ -7,8 +7,10 @@ import org.aspectj.weaver.ast.And;
 
 import business.data.Day;
 import business.data.Excursion;
+import business.data.Hotel;
 import business.data.SejourCritere;
 import business.data.Site;
+import business.data.Transport;
 import business.data.Visit;
 
 public class Sejour {
@@ -31,30 +33,65 @@ public class Sejour {
 	private List<Visit> visit = new ArrayList<Visit>();
 	private List<Day> weekDays = new ArrayList<Day>();
 	private List<Excursion> listExcursions = new ArrayList<Excursion>();
+	private Hotel lasHotel = null;
+	private int duration = 4;
+	private int effort = 0;
 
 	public void createJourney() {
 		int excursionPrice = criterion.getPrice() / 2; // a modifier pour le rendre plus intélligent
-		// on va récupréer la requête des sites possible 
+		// on va récupréer la requête des sites possible
 		ArrayList<Site> sites = null;
+		ArrayList<Site> sitesMVP = null;
 		if (criterion.getEffortLevel() < 5) {
 			while (listExcursions.size() < 7 && excursionPrice != 0) {
-				listExcursions.add(createExcursion( sites));
+				duration = criterion.getDuration();
+				effort = criterion.getEffortLevel();
+				if (sitesMVP.size() != 0) {
+					listExcursions.add(findExcursion(sitesMVP));
+				} else {
+					listExcursions.add(findExcursion(sites));
+				}
 			}
 		} else {
 			while (listExcursions.size() < 14 && excursionPrice != 0) {
-				listExcursions.add(createExcursion(sites));
+				duration = criterion.getDuration();
+				effort = criterion.getEffortLevel();
+				if (sitesMVP.size() != 0) {
+					listExcursions.add(findExcursion(sitesMVP));
+				} else {
+					listExcursions.add(findExcursion(sites));
+				}
 			}
 		}
 
 	}
 
-	public Excursion createExcursion(ArrayList<Site> site) {
-		if (criterion.getEffortLevel() < 5) {
+	// @ todo prendre en compte les prix aussi + ajouter le transport
 
-			
+	private Excursion findExcursion(ArrayList<Site> sites) {
+		Excursion bestExcursion = new Excursion();
+		int index = 0;
+		int newactivity = 0;
+		if (duration - sites.get(index).getDuration() >= 0 && effort - sites.get(index).getEffort() >= 0) {
+			bestExcursion.addSite(sites.get(index));
+			duration = duration - sites.get(index).getDuration();
+			effort = effort - sites.get(index).getEffort();
+			newactivity++;
+			sites.remove(index);
 		} else {
+			index++;
+		}
+		if (duration <= 0 || effort <= 0) {
+			if (lasHotel != null) {
+				lasHotel = bestExcursion.getHotelList().get(newactivity);
+			} else {
+				lasHotel = bestExcursion.getHotelList().get(newactivity - 1);
+
+			}
 
 		}
-		return null;
+
+		return bestExcursion;
 	}
+
 }
