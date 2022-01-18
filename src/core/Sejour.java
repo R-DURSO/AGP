39287f68b49e2b37b02c4414da6e5ba749156076
R-DURSO <rdurso@outlook.fr>
2022-01-18 15:19:13 +0100

    maj séjour Version 1

--- src/core/Sejour.java
+++ src/core/Sejour.java
@@ -18,15 +18,15 @@ public class Sejour {
 
 	};
 
-	public List<ArrayList<Visit>> getJourney() {
+	public List<ArrayList<Day>> getJourney() {
 		return journey;
 	}
 
-	public void setJourney(List<ArrayList<Visit>> journey) {
+	public void setJourney(List<ArrayList<Day>> journey) {
 		this.journey = journey;
 	}
 
-	private List<ArrayList<Visit>> journey = new ArrayList<ArrayList<Visit>>();
+	private List<ArrayList<Day>> journey = new ArrayList<ArrayList<Day>>();
 	private SejourCritere criterion;
 	private int journeyLong = 7;
 
@@ -36,38 +36,56 @@ public class Sejour {
 	private Hotel lasHotel = null;
 	private int duration = 4;
 	private int effort = 0;
-
+	private int averageHotelPrice = 0 ; 
+	private int moneyRemaining = 0;
+	private int numberHotelNight = 7;
+	private int indexOfPriority = 0 ;
+	/**
+	 * createJourney use 3 methode on this order
+	 * the findExucrsion will be use many time for create a list of excursion
+	 * selectday will fill the 7 doy the journey
+	 * create journey will storing on the week
+	 */
 	public void createJourney() {
-		int excursionPrice = criterion.getPrice() / 2; // a modifier pour le rendre plus intélligent
-		// on va récupréer la requête des sites possible
+		// ajout la quÃªte pour le prix moyen de l'hotel 
+		moneyRemaining = criterion.getPrice() - 15 - numberHotelNight * averageHotelPrice; // a modifier pour le rendre plus intÃ©lligent
+		// on va rÃ©cuprÃ©er la requete des sites possible
 		ArrayList<Site> sites = null;
 		ArrayList<Site> sitesMVP = null;
 		if (criterion.getEffortLevel() < 5) {
-			while (listExcursions.size() < 7 && excursionPrice != 0) {
+			while (listExcursions.size() < 7 && moneyRemaining != 0) {
 				duration = criterion.getDuration();
 				effort = criterion.getEffortLevel();
 				if (sitesMVP.size() != 0) {
 					listExcursions.add(findExcursion(sitesMVP));
+					indexOfPriority = listExcursions.size();
+					
 				} else {
 					listExcursions.add(findExcursion(sites));
 				}
 			}
 		} else {
-			while (listExcursions.size() < 14 && excursionPrice != 0) {
+			while (listExcursions.size() < 14 && moneyRemaining != 0) {
 				duration = criterion.getDuration();
 				effort = criterion.getEffortLevel();
 				if (sitesMVP.size() != 0) {
 					listExcursions.add(findExcursion(sitesMVP));
-				} else {
+					indexOfPriority = listExcursions.size();
+					} else {
 					listExcursions.add(findExcursion(sites));
 				}
 			}
 		}
 
+		creationWorkingTime();
 	}
 
 	// @ todo prendre en compte les prix aussi + ajouter le transport
-
+	/**
+	 * 
+	 * @param sites  a list of different possible to go during this journey 
+	 * @return a best 
+	 */
 	private Excursion findExcursion(ArrayList<Site> sites) {
 		Excursion bestExcursion = new Excursion();
 		int index = 0;
@@ -85,14 +103,38 @@ public class Sejour {
 			if (duration <= 0 || effort <= 0) {
 				if (lasHotel != null) {
 					lasHotel = bestExcursion.getHotelList().get(newactivity);
+					return bestExcursion;
 				} else {
 					lasHotel = bestExcursion.getHotelList().get(newactivity - 1);
-
+					return bestExcursion;
 				}
 
 			}
 		}
-		return bestExcursion;
+		bestExcursion.addHotel(lasHotel);
+		bestExcursion.addSite(null);
+		return bestExcursion; 
 	}
-
+	/**
+	 * repartion of the different excursion possible 
+	 */
+public void creationWorkingTime(){
+	while(listExcursions.size() != 0 ) {
+		int i = 0 ;
+		if(effort  <=5) {
+			for(Day days : weekDays ) {
+					days.getExcursionList().add(listExcursions.get(i));
+					i++;
+			}
+		}else {
+			for(Day days : weekDays ) {
+				days.getExcursionList().add(listExcursions.get(i));
+				i++;
+				days.getExcursionList().add(listExcursions.get(i));
+				i++;
+		}
+		}
+	}
+	journey.add((ArrayList<Day>) weekDays);
+}
 }
