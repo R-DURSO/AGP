104f1a6bb671fe574682703b4056fa630cec256c
Mathis <mathi@LAPTOP-FAC08LFA>
2022-01-21 10:38:39 +0100

    Clean

--- src/test/TestAlgo.java
+++ src/test/TestAlgo.java
@@ -2,30 +2,27 @@ package test;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.lucene.queryparser.classic.ParseException;
 
 import business.core.Journey;
-import business.data.Day;
 import business.data.Excursion;
 import business.data.JourneyCritere;
 
 
 public class TestAlgo {
 	public static void main(String[] args) throws IOException, ParseException, SQLException{
-		JourneyCritere crit = new JourneyCritere(1, 3000, 7, "Cascades rivière", 5, 5);
+		int confort = 1;
+		int price = 3000;
+		int duration = 7;
+		String keyWord = "Cascades rivière";
+		int frequency = 5;
+		int effort = 5;
+		
+		JourneyCritere crit = new JourneyCritere(confort, price, duration, keyWord, frequency, effort);
 		Journey journey = new Journey();
-		//journey.setCritere(crit);
-		//System.out.println(expJourney.getCritere().getEffortLevel());
 		journey.init(crit);
-//		List<Day>  days = journey.getWeek();
-//		System.out.println(days.size());
-//		for (Day day : days) {
-//			System.out.println(day);
-//		}
-//		System.out.println((int)0.9*14);
 		
 		System.out.println("====== Résultat ========");
 		List<Excursion> excursions = journey.getJourney();
