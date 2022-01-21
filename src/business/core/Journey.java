11fb4001adc9d46356e1d0327ecbaaf452d2da32
Mathis <mathi@LAPTOP-FAC08LFA>
2022-01-21 11:12:01 +0100

    Javadoc journey

--- src/business/core/Journey.java
+++ src/business/core/Journey.java
@@ -12,6 +12,10 @@ import business.data.Site;
 import persistence.jdbc.JdbcPersistence;
 import persistence.jdbc.SiteScore;
 
+/**
+ * Journey manages the creation of the whole journey which consists of multiple excursions.
+ * It also creates the excursions.
+ */
 public class Journey {
 
 	JdbcPersistence persistence = new JdbcPersistence();
@@ -19,7 +23,7 @@ public class Journey {
 	private final int EXCURSION_DURATION = 4;
 	private List<Excursion> journey = new ArrayList<>();
 	
-	// journeyDuration -> half-day
+	// journeyDuration in half-days
 	private int journeyDuration = 14;
 	private int journeyPrice = 0;
 	private int budget;
@@ -27,7 +31,7 @@ public class Journey {
 	private int frequency;
 	private String keyWord;
 	
-	// Iterators
+	// Iterators that comes from the db
 	private Iterator<Hotel> hotelIterator;
 	private Iterator<SiteScore> siteIterator;	
 	private Iterator<Site> otherSiteIterator;
@@ -51,6 +55,10 @@ public class Journey {
 	public Journey() {
 	}
 	
+	/**
+	 * Initialise the criterions and get data from the db
+	 * @param critere
+	 */
 	public void init(JourneyCritere critere) {
 		this.critere = critere;
 		this.budget = critere.getPrice();
@@ -63,10 +71,12 @@ public class Journey {
 		siteIterator = persistence.getTouristAttractionWithKeyWord(words[0], words[1]);
 		otherSiteIterator = persistence.allTouristAttractions();
 		hotelIterator = persistence.getAllHotel(confort);
+		
 		hotels = new ArrayList<>();
 		sites = new ArrayList<>();
 		otherSites = new ArrayList<>();
 		
+		// Convert iterators into lists
 		hotelIterator.forEachRemaining(hotels::add);
 		siteIterator.forEachRemaining(sites::add);
 		otherSiteIterator.forEachRemaining(otherSites::add);
@@ -74,11 +84,12 @@ public class Journey {
 		createJourney();
 	}
 
+	/**
+	 * Creation of the journey. Create multiple excursions some with sites and some without depending on the frequency specified by the user
+	 * Shuffle the order of excursions so the journey is more homogeneous
+	 */
 	public void createJourney() {
-		// on réparti les excursion de facon aléatoire 
 		Collections.shuffle(otherSites);
-		
-		// Collections.shuffle(siteList);
 		
 		// Create excursions with visits
 		List<Excursion> excursions = new ArrayList<Excursion>();
@@ -103,12 +114,18 @@ public class Journey {
 		
 		journey = excursions;
 	}
-		
+	
+	/**
+	 * Create a single excursion. We create excursion with sites that match the user's keywords (in sites) in priority. If needed we use
+	 * otherSites which contains sites that do not match the keywords
+	 * @return an excursion with potentially multiple sites
+	 */
 	public Excursion createExcursion() {
 		Excursion excursion = new Excursion();
 		int totalExcursionTime = 0;
 		List<Site> visitedSites = new ArrayList<>();
 		
+		// Do this when there are still matching sites remaining
 		if(!sites.isEmpty()) {
 			for(Site site : sites) {
 				if(totalExcursionTime + site.getDuration() <= EXCURSION_DURATION && journeyPrice + site.getPrice() <= budget) {
@@ -141,12 +158,18 @@ public class Journey {
 		return excursion;
 	}
 	
-	// Set departure and arrival hotels for each excursion
+	/**
+	 * Add departure and arrival hotels to each excursion. The departure hotel from an excursion is the arrival hotel from the last excursion.
+	 * The arrival hotel is the closest hotel to the last visited site.
+	 * @param excursions
+	 */
 	public void linkHotels(List<Excursion> excursions) {
 		for(int i = 0; i < excursions.size(); i++) {
+			// Process the first excursion differently because it doesn't have any previous excursion
 			if(i == 0) {
 				Excursion firstExcursion = excursions.get(0);
 				List<Site> l = firstExcursion.getSiteList();
+				// If there is no sites to visit for this excursion (ex: chill at the beach) the arrival and departure hotels are the same.
 				if(!l.isEmpty()) {
 					Site firstSite = l.get(0);
 					Hotel firstHotel = JourneyUtility.getClosestHotel(hotels, firstSite);
@@ -177,9 +200,17 @@ public class Journey {
 		}
 	}
 	
+	/**
+	 * Distributes excursions on the whole journey. When we create the excursions, depending on the frequency of visites we might have excursions
+	 * with no visits at all. We distribute the excursions so as to not have all the visits during the first part of the journey and no excursions
+	 * in the second part of the journey.
+	 * @param excursions
+	 * @return a list of excursions
+	 */
 	public List<Excursion> excursionDistribution(List<Excursion> excursions) {
 		int priorityNumber = excursions.size();
         
+		// Shuffle randomly or distribute depending on the effort level to have more even days effort wise using an average effort of activities for the day (a day = 2 excursions)
         if (confort < 3) {
             Collections.shuffle(excursions);
         } else {
